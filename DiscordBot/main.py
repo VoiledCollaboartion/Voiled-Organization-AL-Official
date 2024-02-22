db06c39ec327b19af5e1898cff93ac93abec6bba
Tony <159955366+tonyrav3n@users.noreply.github.com>
2024-02-22 17:55:13 +0100

    Add files via upload

--- DiscordBot/main.py
+++ DiscordBot/main.py
@@ -1,8 +1,8 @@
+import asyncio
+import os
+
 import discord
 from discord.ext import commands
-import os
-import asyncio
-
 
 from setup import *
 
@@ -36,6 +36,3 @@ async def main():
 
 
 asyncio.run(main())
-
-
-# coded by doyletony
