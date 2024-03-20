117f15dfddc4f78b8adea108ad1c16c512a2d88e
Tony <159955366+tonyrav3n@users.noreply.github.com>
2024-03-20 11:36:49 +0100

    Made a few changes

--- DiscordBot/setup.py
+++ DiscordBot/setup.py
@@ -1,14 +1,6 @@
 # settings
-TOKEN = "" # Input token here
-PREFIX = "/" # Default prefix
+TOKEN = "MTE5NDIxNjEwMzg1ODE1OTY2Ng.G6rsRl.6_yFbfaoDJFyyaqb7SonK_TQaKP0Si28JqqC9U"
+PREFIX = "/"
 
 # Input appropriate IDs and contents.
-owner_id = 0
-WELCOME_CHANNEL_ID = 0
-PROGRESS_CHANNEL_ID = 0
-VERIFICATION_CHANNEL_ID = 0
-RULES_CHANNEL_ID = 0
-CHAT_CHANNEL_ID = 0
-VERIFIED_ROLE_NAME = ""
-UNVERIFIED_ROLE_NAME = ""
-PREMIUM_ROLE_NAME = ""
+owner_id = 643962985089335296
