bd125303a038c4bc725f7e0b0bfa60a50d82ce57
Athen Lee <cupid.jack505@gmail.com>
2024-09-24 04:17:59 -0400

    layout add

--- voiled-cert-temp-frontend/app/layout.js
+++ voiled-cert-temp-frontend/app/layout.js
@@ -1,5 +1,10 @@
 import localFont from "next/font/local";
+
+import { Divider, Menu } from "antd";
+import { SolutionOutlined, ReadOutlined, HistoryOutlined, StarOutlined, GroupOutlined, IdcardOutlined } from "@ant-design/icons";
+
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -18,12 +23,54 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const items = [
+    {
+      key: "profile",
+      icon: <SolutionOutlined />,
+      label: <Link href={"/resume/profile/add"}>Profile</Link>
+    },
+    {
+      key: "education",
+      icon: <ReadOutlined />,
+      label: <Link href={"/resume/profile/add"}>Education</Link>
+    },
+    {
+      key: "experience",
+      icon: <HistoryOutlined />,
+      label: <Link href={"/resume/profile/add"}>Experience</Link>
+    },
+    {
+      key: "skill",
+      icon: <StarOutlined />,
+      label: <Link href={"/resume/profile/add"}>Skill</Link>
+    },
+    {
+      key: "group",
+      icon: <GroupOutlined />,
+      label: <Link href={"/resume/profile/add"}>Freelancer</Link>
+    },
+    {
+      key: "cert",
+      icon: <IdcardOutlined />,
+      label: <Link href={"/resume/profile/add"}>Certification</Link>
+    },
+  ]
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="flex flex-row">
+          <div className="w-72 border-r">
+            <h2 className="text-xl font-black text-center pt-6">Voiled</h2>
+            <Divider />
+            <Menu items={items} mode="inline" style={{borderRight: 'none'}}/>
+          </div>
+          <div className="flex-1">
+            {children}
+          </div>
+        </div>
       </body>
     </html>
   );
