8c8de6a716d7a68669b0bf1d6655b8b06f576b1d
Athen Lee <cupid.jack505@gmail.com>
2024-09-24 09:05:31 -0400

    cert backend mail transfer test success

--- voiled-cert-backend/src/services/email.service.js
+++ voiled-cert-backend/src/services/email.service.js
@@ -3,13 +3,15 @@ const config     = require('../config/config');
 const logger     = require('../config/logger');
 
 const transport = nodemailer.createTransport(config.email.smtp);
+
 /* istanbul ignore next */
-if (config.env !== 'test') {
-  transport
-    .verify()
-    .then(() => logger.info('Connected to email server'))
-    .catch(() => logger.warn('Unable to connect to email server. Make sure you have configured the SMTP options in .env'));
-}
+// if (config.env !== 'test') {
+//   console.log(config.email.smtp) ;
+//   transport
+//     .verify()
+//     .then(() => logger.info('Connected to email server'))
+//     .catch(() => logger.warn('Unable to connect to email server. Make sure you have configured the SMTP options in .env'));
+// }
 
 /**
  * Send an email
